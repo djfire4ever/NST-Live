cc468031333836d609a22ce5a27ced4a6088abb4
djfire4ever <djfire4ever@gmail.com>
2025-06-10 10:09:29 -0400

    site-fix

--- calendar.js
+++ calendar.js
@@ -305,3 +305,4 @@ function formatDateTimeLocal(date) {
   const pad = n => String(n).padStart(2, '0');
   return `${date.getFullYear()}-${pad(date.getMonth() + 1)}-${pad(date.getDate())}T${pad(date.getHours())}:${pad(date.getMinutes())}`;
 }
+//
